4eb19652be8c401b0307db2caccfd92400ddcb49
Duy Bảo <duybao05123@gmail.com>
2020-02-24 15:28:48 +0700

    update home tab

--- Trading View/Trading View/View/ComponentView/NewsCellView.swift
+++ Trading View/Trading View/View/ComponentView/NewsCellView.swift
@@ -7,14 +7,36 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct NewsCellView: View {
     var news: NewsModel
     var body: some View {
         NavigationLink(destination: DetailView(url: news.slugUrl)) {
                 HStack {
-                    Text(news.vi?.title ?? "nil")
-                    Text(news.slugUrl ?? "nil")
+                    VStack {
+                        WebImage(url: URL(string: "\(Constant.API.baseHostAPI)\(news.thumbnail?.filename ?? "")"))
+                            .onSuccess { image, cacheType in
+                                // Success
+                            }
+                            .resizable() // Resizable like SwiftUI.Image
+                            .placeholder(Image(systemName: "photo")) // Placeholder Image
+                            // Supports ViewBuilder as well
+                            .placeholder {
+                                Rectangle().foregroundColor(.gray)
+                            }
+                            .indicator(.activity) // Activity Indicator
+                            .animation(.easeInOut(duration: 0.5)) // Animation Duration
+                            .transition(.fade) // Fade Transition
+                            .frame(width: 150, height: 100, alignment: .center)
+                        Text(String(news.publishedTime ?? 0))
+                    }
+                    VStack {
+                        Text("Tài Chính - Ngân Hàng")
+                            .foregroundColor(.gray)
+                            .font(.subheadline)
+                        Text(news.vi?.title ?? "nil")
+                    }
             }
         }
     }
